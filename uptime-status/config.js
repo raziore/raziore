157a7e0ae0f97b77a7e76558ee60d983c4633bf0
Fashion <ocarno@outlook.com>
2023-01-06 15:37:32 +0800

    Add files via upload

---
+++ uptime-status/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Ocarno',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1966197-ced2575272f18ff6b40f6d9e',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://ocarno.netlify.app/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/snvike/vike'
+    },
+  ],
+};
